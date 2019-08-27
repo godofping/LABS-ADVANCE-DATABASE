f1193f38670fcfb36aba5e8d45776f8fb9747d04
D Murphy <rexmicrosoft@yahoo.com.ph>
2019-08-28 05:56:57 +0800

    firstlab

---
+++ Laboratory1/Laboratory1/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace Laboratory1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.txtGrade = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.btnSubmit.Font = new System.Drawing.Font("Gill Sans MT", 18F);
+            this.btnSubmit.Location = new System.Drawing.Point(33, 158);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(135, 65);
+            this.btnSubmit.TabIndex = 0;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = false;
+            this.btnSubmit.Click += new System.EventHandler(this.BtnSubmit_Click);
+            // 
+            // txtGrade
+            // 
+            this.txtGrade.Font = new System.Drawing.Font("Gill Sans MT", 18F);
+            this.txtGrade.Location = new System.Drawing.Point(33, 104);
+            this.txtGrade.Name = "txtGrade";
+            this.txtGrade.Size = new System.Drawing.Size(262, 35);
+            this.txtGrade.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Gill Sans MT", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(27, 52);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(138, 34);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Enter Grade";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
+            this.ClientSize = new System.Drawing.Size(341, 314);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtGrade);
+            this.Controls.Add(this.btnSubmit);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.TextBox txtGrade;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Laboratory1/Laboratory1/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Laboratory1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSubmit_Click(object sender, EventArgs e)
+        {
+            int intGrade = Convert.ToInt32(txtGrade.Text);
+            String strResult;
+
+            if (intGrade <= 100 && intGrade >= 95)
+            {
+                strResult = "A";
+            }
+            else if (intGrade <= 95 && intGrade >= 90)
+            {
+                strResult = "B";
+            }
+            else if (intGrade <= 90 && intGrade >= 85)
+            {
+                strResult = "C";
+            }
+            else if (intGrade <= 85 && intGrade >= 80)
+            {
+                strResult = "D";
+            }
+            else if (intGrade <= 80 && intGrade >= 75)
+            {
+                strResult = "E";
+            }
+            else if (intGrade <= 75 && intGrade >= 70)
+            {
+                strResult = "F";
+            }
+            else
+            {
+                strResult = "Invalid";
+            }
+
+            MessageBox.Show("Your grade is " + strResult);
+        }
+    }
+}
