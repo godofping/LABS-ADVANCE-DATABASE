0f99177bbc8f23d9a7581bb84889716e0ded3b52
D Murphy <rexmicrosoft@yahoo.com.ph>
2019-12-09 14:36:48 +0800

    a

--- purchase order system/early/student/student.PL/Registrations/frmRegisteredStudents.Designer.cs
+++ purchase order system/early/student/student.PL/Registrations/frmRegisteredStudents.Designer.cs
@@ -29,10 +29,10 @@
         private void InitializeComponent()
         {
             this.pnlCRUD = new System.Windows.Forms.Panel();
-            this.txtDescription = new System.Windows.Forms.TextBox();
+            this.txtLastName = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.lblTitle = new System.Windows.Forms.Label();
-            this.txtStrand = new System.Windows.Forms.TextBox();
+            this.txtLRN = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
             this.btnSave = new System.Windows.Forms.Button();
             this.btnCancel = new System.Windows.Forms.Button();
@@ -41,6 +41,24 @@
             this.txtSearch = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.dgv = new System.Windows.Forms.DataGridView();
+            this.txtFirstName = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtMiddleInitial = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtAddress = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtParentsOrGuardian = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtContactNumber = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtLastSchoolAttended = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtSchoolYear = new System.Windows.Forms.TextBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.cbPreferredStrand = new System.Windows.Forms.ComboBox();
+            this.cbYearLevel = new System.Windows.Forms.ComboBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
             this.pnlCRUD.SuspendLayout();
             this.pnlMain.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.dgv)).BeginInit();
@@ -48,10 +66,28 @@
             // 
             // pnlCRUD
             // 
-            this.pnlCRUD.Controls.Add(this.txtDescription);
+            this.pnlCRUD.Controls.Add(this.label12);
+            this.pnlCRUD.Controls.Add(this.label11);
+            this.pnlCRUD.Controls.Add(this.cbYearLevel);
+            this.pnlCRUD.Controls.Add(this.cbPreferredStrand);
+            this.pnlCRUD.Controls.Add(this.txtSchoolYear);
+            this.pnlCRUD.Controls.Add(this.label10);
+            this.pnlCRUD.Controls.Add(this.txtLastSchoolAttended);
+            this.pnlCRUD.Controls.Add(this.label9);
+            this.pnlCRUD.Controls.Add(this.txtContactNumber);
+            this.pnlCRUD.Controls.Add(this.label8);
+            this.pnlCRUD.Controls.Add(this.txtParentsOrGuardian);
+            this.pnlCRUD.Controls.Add(this.label7);
+            this.pnlCRUD.Controls.Add(this.txtAddress);
+            this.pnlCRUD.Controls.Add(this.label6);
+            this.pnlCRUD.Controls.Add(this.txtMiddleInitial);
+            this.pnlCRUD.Controls.Add(this.label5);
+            this.pnlCRUD.Controls.Add(this.txtFirstName);
+            this.pnlCRUD.Controls.Add(this.label4);
+            this.pnlCRUD.Controls.Add(this.txtLastName);
             this.pnlCRUD.Controls.Add(this.label3);
             this.pnlCRUD.Controls.Add(this.lblTitle);
-            this.pnlCRUD.Controls.Add(this.txtStrand);
+            this.pnlCRUD.Controls.Add(this.txtLRN);
             this.pnlCRUD.Controls.Add(this.label2);
             this.pnlCRUD.Controls.Add(this.btnSave);
             this.pnlCRUD.Controls.Add(this.btnCancel);
@@ -60,21 +96,21 @@
             this.pnlCRUD.Size = new System.Drawing.Size(984, 537);
             this.pnlCRUD.TabIndex = 8;
             // 
-            // txtDescription
+            // txtLastName
             // 
-            this.txtDescription.Location = new System.Drawing.Point(7, 118);
-            this.txtDescription.Name = "txtDescription";
-            this.txtDescription.Size = new System.Drawing.Size(335, 20);
-            this.txtDescription.TabIndex = 12;
+            this.txtLastName.Location = new System.Drawing.Point(7, 118);
+            this.txtLastName.Name = "txtLastName";
+            this.txtLastName.Size = new System.Drawing.Size(335, 20);
+            this.txtLastName.TabIndex = 12;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Location = new System.Drawing.Point(4, 101);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(87, 13);
+            this.label3.Size = new System.Drawing.Size(75, 13);
             this.label3.TabIndex = 11;
-            this.label3.Text = "DESCRIPTION *";
+            this.label3.Text = "LAST NAME *";
             // 
             // lblTitle
             // 
@@ -85,21 +121,21 @@
             this.lblTitle.TabIndex = 10;
             this.lblTitle.Text = "<>";
             // 
-            // txtStrand
+            // txtLRN
             // 
-            this.txtStrand.Location = new System.Drawing.Point(7, 72);
-            this.txtStrand.Name = "txtStrand";
-            this.txtStrand.Size = new System.Drawing.Size(335, 20);
-            this.txtStrand.TabIndex = 9;
+            this.txtLRN.Location = new System.Drawing.Point(7, 72);
+            this.txtLRN.Name = "txtLRN";
+            this.txtLRN.Size = new System.Drawing.Size(335, 20);
+            this.txtLRN.TabIndex = 9;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.Location = new System.Drawing.Point(4, 55);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(59, 13);
+            this.label2.Size = new System.Drawing.Size(36, 13);
             this.label2.TabIndex = 8;
-            this.label2.Text = "STRAND *";
+            this.label2.Text = "LRN *";
             // 
             // btnSave
             // 
@@ -109,6 +145,7 @@
             this.btnSave.TabIndex = 7;
             this.btnSave.Text = "SAVE";
             this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
             // 
             // btnCancel
             // 
@@ -118,6 +155,7 @@
             this.btnCancel.TabIndex = 6;
             this.btnCancel.Text = "CANCEL";
             this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
             // 
             // pnlMain
             // 
@@ -138,6 +176,7 @@
             this.btnAdd.TabIndex = 6;
             this.btnAdd.Text = "ADD";
             this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
             // 
             // txtSearch
             // 
@@ -145,6 +184,7 @@
             this.txtSearch.Name = "txtSearch";
             this.txtSearch.Size = new System.Drawing.Size(279, 20);
             this.txtSearch.TabIndex = 5;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
             // 
             // label1
             // 
@@ -167,14 +207,161 @@
             this.dgv.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
             this.dgv.Size = new System.Drawing.Size(978, 499);
             this.dgv.TabIndex = 3;
+            this.dgv.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgv_CellContentClick);
+            // 
+            // txtFirstName
+            // 
+            this.txtFirstName.Location = new System.Drawing.Point(7, 162);
+            this.txtFirstName.Name = "txtFirstName";
+            this.txtFirstName.Size = new System.Drawing.Size(335, 20);
+            this.txtFirstName.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(4, 145);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(79, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "FIRST NAME *";
+            // 
+            // txtMiddleInitial
+            // 
+            this.txtMiddleInitial.Location = new System.Drawing.Point(7, 208);
+            this.txtMiddleInitial.Name = "txtMiddleInitial";
+            this.txtMiddleInitial.Size = new System.Drawing.Size(335, 20);
+            this.txtMiddleInitial.TabIndex = 16;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(4, 191);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(95, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "MIDDLE INITIAL *";
+            // 
+            // txtAddress
+            // 
+            this.txtAddress.Location = new System.Drawing.Point(7, 253);
+            this.txtAddress.Name = "txtAddress";
+            this.txtAddress.Size = new System.Drawing.Size(335, 20);
+            this.txtAddress.TabIndex = 18;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(4, 236);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(66, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "ADDRESS *";
+            // 
+            // txtParentsOrGuardian
+            // 
+            this.txtParentsOrGuardian.Location = new System.Drawing.Point(7, 300);
+            this.txtParentsOrGuardian.Name = "txtParentsOrGuardian";
+            this.txtParentsOrGuardian.Size = new System.Drawing.Size(335, 20);
+            this.txtParentsOrGuardian.TabIndex = 20;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(4, 283);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(137, 13);
+            this.label7.TabIndex = 19;
+            this.label7.Text = "PARENTS OR GUARDIAN";
+            // 
+            // txtContactNumber
+            // 
+            this.txtContactNumber.Location = new System.Drawing.Point(7, 346);
+            this.txtContactNumber.Name = "txtContactNumber";
+            this.txtContactNumber.Size = new System.Drawing.Size(335, 20);
+            this.txtContactNumber.TabIndex = 22;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(4, 329);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(115, 13);
+            this.label8.TabIndex = 21;
+            this.label8.Text = "CONTACT NUMBER *";
+            // 
+            // txtLastSchoolAttended
+            // 
+            this.txtLastSchoolAttended.Location = new System.Drawing.Point(7, 391);
+            this.txtLastSchoolAttended.Name = "txtLastSchoolAttended";
+            this.txtLastSchoolAttended.Size = new System.Drawing.Size(335, 20);
+            this.txtLastSchoolAttended.TabIndex = 24;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(4, 374);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(150, 13);
+            this.label9.TabIndex = 23;
+            this.label9.Text = "LAST SCHOOL ATTENDED *";
+            // 
+            // txtSchoolYear
+            // 
+            this.txtSchoolYear.Location = new System.Drawing.Point(7, 436);
+            this.txtSchoolYear.Name = "txtSchoolYear";
+            this.txtSchoolYear.Size = new System.Drawing.Size(335, 20);
+            this.txtSchoolYear.TabIndex = 26;
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(4, 419);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(90, 13);
+            this.label10.TabIndex = 25;
+            this.label10.Text = "SCHOOL YEAR *";
+            // 
+            // cbPreferredStrand
+            // 
+            this.cbPreferredStrand.FormattingEnabled = true;
+            this.cbPreferredStrand.Location = new System.Drawing.Point(423, 71);
+            this.cbPreferredStrand.Name = "cbPreferredStrand";
+            this.cbPreferredStrand.Size = new System.Drawing.Size(335, 21);
+            this.cbPreferredStrand.TabIndex = 27;
+            // 
+            // cbYearLevel
+            // 
+            this.cbYearLevel.FormattingEnabled = true;
+            this.cbYearLevel.Location = new System.Drawing.Point(423, 117);
+            this.cbYearLevel.Name = "cbYearLevel";
+            this.cbYearLevel.Size = new System.Drawing.Size(335, 21);
+            this.cbYearLevel.TabIndex = 28;
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(420, 55);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(128, 13);
+            this.label11.TabIndex = 29;
+            this.label11.Text = "PREFERRED STRAND *";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(420, 101);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(79, 13);
+            this.label12.TabIndex = 30;
+            this.label12.Text = "YEAR LEVEL *";
             // 
             // frmRegisteredStudents
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1008, 561);
-            this.Controls.Add(this.pnlMain);
             this.Controls.Add(this.pnlCRUD);
+            this.Controls.Add(this.pnlMain);
             this.Name = "frmRegisteredStudents";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "REGISTERED STUDENTS";
@@ -191,10 +378,10 @@
         #endregion
 
         private System.Windows.Forms.Panel pnlCRUD;
-        private System.Windows.Forms.TextBox txtDescription;
+        private System.Windows.Forms.TextBox txtLastName;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label lblTitle;
-        private System.Windows.Forms.TextBox txtStrand;
+        private System.Windows.Forms.TextBox txtLRN;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button btnSave;
         private System.Windows.Forms.Button btnCancel;
@@ -203,5 +390,23 @@
         private System.Windows.Forms.TextBox txtSearch;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.DataGridView dgv;
+        private System.Windows.Forms.TextBox txtContactNumber;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtParentsOrGuardian;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtAddress;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtMiddleInitial;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtFirstName;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtSchoolYear;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox txtLastSchoolAttended;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.ComboBox cbYearLevel;
+        private System.Windows.Forms.ComboBox cbPreferredStrand;
     }
 }
--- purchase order system/early/student/student.PL/Registrations/frmRegisteredStudents.cs
+++ purchase order system/early/student/student.PL/Registrations/frmRegisteredStudents.cs
@@ -17,13 +17,101 @@ namespace student.PL.Registrations
         BL.Registrations.strands strandBL = new student.BL.Registrations.strands();
         BL.Registrations.students studentBL = new student.BL.Registrations.students();
 
+        string s = "";
 
         public frmRegisteredStudents()
         {
             InitializeComponent();
         }
 
+        private void ClearControls()
+        {
+            methods.ClearTXT(txtLRN, txtLastName, txtFirstName, txtMiddleInitial, txtAddress, txtParentsOrGuardian, txtContactNumber, txtLastSchoolAttended, txtSchoolYear);
+            methods.ClearCB(cbPreferredStrand, cbYearLevel);
+        }
+
+        private void ShowForm(bool bol)
+        {
+            pnlMain.Visible = !bol;
+            pnlCRUD.Visible = bol;
+
+            ClearControls();
+        }
+
+        private void PopulateDGV()
+        {
+            methods.LoadDGV(dgv, studentBL.List(txtSearch.Text));
+            methods.DGVHiddenColumns(dgv, "STUDENT ID","STRAND ID", "C");
+        }
+
+        private void ShowResult(bool bol)
+        {
+            if (bol)
+            {
+                PopulateDGV();
+                MessageBox.Show("SUCCESS");
+            }
+            else
+            {
+                MessageBox.Show("FAILED");
+            }
+        }
+
+
         private void frmRegisteredStudents_Load(object sender, EventArgs e)
+        {
+            ShowForm(false);
+            PopulateDGV();
+            methods.DGVAddButtons(dgv);
+            PopulateDGV();
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (methods.CheckRequiredTXT(txtStrand, txtDescription))
+            {
+                bool bol = false;
+
+                strandEL.Strand = txtStrand.Text;
+                strandEL.Stranddescription = txtDescription.Text;
+
+                if (s.Equals("ADD"))
+                {
+                    bol = strandBL.Insert(strandEL) > 0;
+                }
+                else if (s.Equals("EDIT"))
+                {
+                    bol = strandBL.Update(strandEL);
+                }
+
+                ShowForm(false);
+                ShowResult(bol);
+
+
+            }
+            else
+            {
+                MessageBox.Show("PLEASE FILL OUT THE FIELDS MARKED WITH AN ASTERISK");
+            }
+
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dgv_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
 
         }
