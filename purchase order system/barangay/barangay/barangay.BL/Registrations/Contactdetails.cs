986aaa527f42fff7ec5b12dc6c5e261a668d9ce9
D Murphy <rexmicrosoft@yahoo.com.ph>
2019-12-22 11:08:20 +0800

    done adding bl dl el

---
+++ purchase order system/barangay/barangay/barangay.BL/Registrations/Contactdetails.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace barangay.BL.Registrations
+{
+    public class Contactdetails
+    {
+    }
+}
